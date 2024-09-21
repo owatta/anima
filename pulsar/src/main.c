1d8793f11f64fbbe5e9bd8d85e8d750ceb8f111e
owatta <arsenii.korniec@bk.ru>
2024-09-21 13:58:30 +0500

    Got some stuff working.
    So I've been staring at the spec for a few days now, trying to figure
    out how to map types in the spec to C types and whatnot. That doesn't
    seem to work. Best thing I could do right now is to make something
    that works, even if it's fundamentally wrong, to avoid burnout and
    solidify some intuitions. So, I did exactly that.

--- pulsar/src/main.c
+++ pulsar/src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
 
+typedef struct Vm {
+  uint8_t memory[0x10000];
+  uint8_t stack[0x100];
+  uint32_t pc;
+} Vm;
+
+Vm vm;
+
+int wasm_eval(uint32_t pc) {
+  for(;;) {
+    switch (vm.memory[pc++]) {
+    case 0x00: {printf("Working!\n"); return 1;};
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stdout, "Usage: %s file.wasm\n", argv[0]);
@@ -12,7 +28,10 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "ERROR: %s\n", argv[1]);
     return 0;
   }
-  char* buf;
-  fread(buf, 0xff00, 1, f), fclose(f);
+
+  vm.pc = 0;
+  fread(vm.memory, 0xff00, 1, f), fclose(f);
+
+  wasm_eval(vm.pc);
   return 0;
 }
